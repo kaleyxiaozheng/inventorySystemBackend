866a1d337902b627e5d655248aa107b17366af58
kaleyxiaozheng <zheng.kaley.x@gmail.com>
2020-06-10 16:01:48 +1000

    complete createUser, updateUser, deleteUser

--- node-api-postgres/queries.js
+++ node-api-postgres/queries.js
@@ -31,7 +31,7 @@ const getUserById = (request, response) => {
 const createUser = (request, response) => {
     const {user_id, given_name, family_name, user_password, user_email, user_phone, user_type, user_role_id, user_department_id} = request.body
 
-    pool.query('INSERT INTO app_user (user_id, given_name, family_name, user_password, user_email, user_phone, user_type, user_role_id, user_department_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)', [user_id, given_name, family_name, user_password, user_email, user_phone, user_type, user_role_id, user_department_id], (error, results)=> {
+    pool.query('INSERT INTO "app_user" (user_id, given_name, family_name, user_password, user_email, user_phone, user_type, user_role_id, user_department_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)', [user_id, given_name, family_name, user_password, user_email, user_phone, user_type, user_role_id, user_department_id], (error, results)=> {
         if(error){
             throw error
         }
@@ -43,7 +43,7 @@ const updateUser = (request, response) => {
     const user_id = parseInt(request.params.id)
     const {user_phone} = request.body
     
-    pool.query('UPDATE app_user SET user_phone = $1 WHERE user_id = $2', [user_phone], (error, results)=> {
+    pool.query('UPDATE "app_user" SET user_phone = $1 WHERE user_id = $2', [user_phone], (error, results)=> {
         if(error){
             throw error
         }
@@ -54,7 +54,7 @@ const updateUser = (request, response) => {
 const deleteUser = (request, response) => {
     const user_id = parseInt(request.params.id)
     
-    pool.query('DELETE FROM app_user WHERE user_id = $1', [user_id], (error, results)=> {
+    pool.query('DELETE FROM "app_user" WHERE user_id = $1', [user_id], (error, results)=> {
         if(error){
             throw error
         }
